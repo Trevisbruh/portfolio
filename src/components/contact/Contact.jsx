66908da7543a9688f6b8facd3e6493be0090cbd5
Khant Thit Oo <160450051+Trevisbruh@users.noreply.github.com>
2024-04-18 22:20:04 +0630

    Add files via upload

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,9 +7,10 @@ const Contact = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
+    e.preventDefault();
 
     emailjs
-      .sendForm('service_sk1zzq9', 'template_30x2vio', form.current, {
+      .sendForm('service_3shmsqh', 'template_30x2vio', form.current, {
         publicKey: '518r1B-HX0__HbZaY',
       })
       .then(
